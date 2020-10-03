0d421f8967d9f36751e539cafb817afb5341429b
Ufuk Arslan <ufuk.arslan@hes-so.ch>
2020-10-03 12:04:57 +0200

    BusinessService

--- src/main/java/com/example/springTuto/topic/TopicController.java
+++ src/main/java/com/example/springTuto/topic/TopicController.java
@@ -3,24 +3,32 @@ package com.example.springTuto.topic;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 
 @RestController
-
 public class TopicController {
 	
-	@RequestMapping("/topics")
+//	Injection de dépendence Autowired permet de regarder dans le registre de spring pour voir les services
+	@Autowired
+	private TopicService topicService;
 	
+	
+	@RequestMapping("/topics")
 //	List retourne automatiquement un fichier converti en json
 	public List<Topic> getAllTopics() {
-		return Arrays.asList(
-				new Topic("spring", "Spring Framework", "Spring Framework Description"),
-				new Topic("java", "Core Java", "Core Java Description"),
-				new Topic("javascript", "JavaScript", "JavaScript Description")
-				);
+		return topicService.getAllTopics();
+	}
+	
+	
+	@RequestMapping("topics/{id}")
+	public Topic getTopic(@PathVariable String id) {
+		return topicService.getTopic(id);
+		
 	}
 
 }
---
+++ src/main/java/com/example/springTuto/topic/TopicService.java
@@ -0,0 +1,29 @@
+package com.example.springTuto.topic;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+	
+	private List<Topic> topics = Arrays.asList(
+			new Topic("spring", "Spring Framework", "Spring Framework Description"),
+			new Topic("java", "Core Java", "Core Java Description"),
+			new Topic("javascript", "JavaScript", "JavaScript Description")
+			);
+	
+	public List<Topic> getAllTopics(){
+		return topics;
+	}
+	
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t->t.getId().equals(id)).findFirst().get();
+//		Nous filtrons la table topics puis nous obtenons les id puis une équivalence avec id passé en paramètre puis prendre le premrier et faire un get dessus
+	}
+	
+	
+	
+
+}
